43352f1fbfe0ddd460884ada2870db260561da7e
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2023-10-27 00:08:19 +0100

    added some features

--- lib/main.dart
+++ lib/main.dart
@@ -21,31 +21,142 @@ class _BookMeFormState extends State<BookMeForm> {
    return MaterialApp(
      debugShowCheckedModeBanner: false,
      home: Scaffold(
+
        appBar: AppBar(
          title: Text("Amazing hairCuts"),
          backgroundColor: Colors.black,
        ),
-       body: Container(
+       body: SafeArea(
            child: SingleChildScrollView(
              child: Column(
                children: [
-                 Image.asset("images/376.jpg", fit: BoxFit.cover, height: 350, width: 450,),
+                 Image.asset("images/376.jpg", fit: BoxFit.cover, height: 280,width: MediaQuery.of(context).size.width,
+                     ),
                  SizedBox(height: 18,),
-                 AnimatedTextKit(
-                   animatedTexts: [
-                     TyperAnimatedText(
-                       "Welcome to our Barbershop, we have great haircuts, amazing beard trims ",
-                       textStyle: TextStyle(
-                         fontSize: 20
-                       )
-                     )
-                   ],
+                 Container(
+                   alignment: Alignment.topLeft,
+                   child: Text.rich(
+                     TextSpan(
+                       text: "hey everyone, we have amazing ",
+                       style: TextStyle( fontSize: 18, ),
+                       children: [
+                         TextSpan(
+                             text: "Haircuts " , style: TextStyle(
+                           fontWeight: FontWeight.bold,
+                         )
+                         ),
+                         TextSpan(
+                             text:  "and so many other great services!, do not Hesitate to book with us ",
+                             style: TextStyle(
+                               height: 3.0,
+                             )
+                         ),
+                       ],
+                     ),
+                   ),
                  ),
-                 const Text("name"),
-                 TextField(
-                   onChanged: (value) {
-                     username = value;
-                   },
+                 SizedBox(height: 12,),
+                 Text("cuts amd Trims ", style: TextStyle(color: Colors.grey, fontSize: 19),),
+                 Card(
+                   child: Column(
+                     children: [
+                       Row(
+                         children: [
+                           Text("Haircuts"),
+                           Spacer(),
+                           Text("£16"),
+                           SizedBox(width: 12,),
+                       Padding(
+                         padding: EdgeInsets.all(12.0),
+                          child:Container(
+                             decoration: BoxDecoration(
+                               borderRadius: BorderRadiusDirectional.circular(30),
+                               color: Colors.blueAccent,
+
+                             ),
+                             child: MaterialButton(
+                               child: Text("book", style: TextStyle(fontWeight: FontWeight.bold),),
+                               onPressed: (){},
+                             ),
+                           ),
+                          ),
+                         ],
+                       ),
+                       Row(
+                         children: [
+                           Text("Beard Trim"),
+                           Spacer(),
+                           Text("£8"),
+                           SizedBox(width: 12,),
+                           Padding(
+                             padding: EdgeInsets.all(12.0),
+                             child:Container(
+                               decoration: BoxDecoration(
+                                 borderRadius: BorderRadiusDirectional.circular(30),
+                                 color: Colors.indigoAccent,
+
+                               ),
+                               child: MaterialButton(
+                                 child: Text("book", style: TextStyle(fontWeight: FontWeight.bold),),
+                                 onPressed: (){},
+                               ),
+                             ),
+                           ),
+                         ],
+                       ),
+                     ],
+                   ),
+                 ),
+                 Text("more services", style: TextStyle(color: Colors.grey, fontSize: 20),),
+                 Card(
+                   child: Column(
+                     children: [
+                       Row(
+                         children: [
+                           Text("Hot Towel"),
+                           Spacer(),
+                           Text("£3"),
+                           SizedBox(width: 12,),
+                           Padding(
+                             padding: EdgeInsets.all(12.0),
+                             child:Container(
+                               decoration: BoxDecoration(
+                                 borderRadius: BorderRadiusDirectional.circular(30),
+                                 color: Colors.teal,
+
+                               ),
+                               child: MaterialButton(
+                                 child: Text("book", style: TextStyle(fontWeight: FontWeight.bold),),
+                                 onPressed: (){},
+                               ),
+                             ),
+                           ),
+                         ],
+                       ),
+                       Row(
+                         children: [
+                           Text("Nose and ear wax"),
+                           Spacer(),
+                           Text("£5"),
+                           SizedBox(width: 12,),
+                           Padding(
+                             padding: EdgeInsets.all(12.0),
+                             child:Container(
+                               decoration: BoxDecoration(
+                                 borderRadius: BorderRadiusDirectional.circular(30),
+                                 color: Colors.grey,
+
+                               ),
+                               child: MaterialButton(
+                                 child: Text("book", style: TextStyle(fontWeight: FontWeight.bold),),
+                                 onPressed: (){},
+                               ),
+                             ),
+                           ),
+                         ],
+                       ),
+                     ],
+                   ),
                  ),
                ],
              ),
