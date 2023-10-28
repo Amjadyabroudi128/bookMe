c89020d43bedf83cc2761710970abb59790e02bf
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2023-10-28 01:06:15 +0100

    added a splash screen

---
+++ lib/Screens/SplashScreen.dart
@@ -0,0 +1,70 @@
+import 'package:bookme/Screens/HomePage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => HomePage(),
+      ));
+    }
+    );
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: SystemUiOverlay.values);
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.cyan.shade900, Colors.black38, Colors.red.shade500],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon( Icons.content_cut_outlined, size: 30 , color: Colors.white,),
+                Icon( Icons.person, size: 30 , color: Colors.white,)
+              ],
+            ),
+            Text(
+              "BookMe",
+              style: TextStyle(
+                fontStyle: FontStyle.italic,
+                fontSize: 32,
+                color: Colors.white
+              ),
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
