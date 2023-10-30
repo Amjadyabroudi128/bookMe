60550fb6f8011175d61315bfc5a66460ccd92235
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2023-10-30 01:37:02 +0000

    async and await firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:bookme/Screens/SplashScreen.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+Future <void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: FirebaseOptions
+        (apiKey: "AIzaSyCg8zJdXr-AAWQ18oTUSJEu1wDXs9jdnbY"
+          , appId: "bookme-ef465", messagingSenderId: "213012481780", projectId: "bookme-ef465")
+  );
   runApp( BookMeForm());
 }
 
