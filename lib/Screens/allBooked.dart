3ca1c8e41d365f4e8732a7287346493a13cc77a2
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2023-10-31 00:20:47 +0000

    added a confirmation screen

---
+++ lib/Screens/allBooked.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class allBooked extends StatelessWidget {
+  const allBooked({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              color: Colors.green,
+              Icons.check_circle_outline, size: 40,
+            ),
+            SizedBox(height: 30,),
+            Text("You are all booked, see you soon!",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.cyan),)
+          ],
+        ) ,
+      ),
+    );
+  }
+}
