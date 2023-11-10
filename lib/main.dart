0cfefdc4dd21c04c883b9eac0db538e2f1ab638e
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2023-11-10 00:13:25 +0000

    .

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,7 @@ class _BookMeFormState extends State<BookMeForm> {
   @override
   Widget build(BuildContext context) {
    return MaterialApp(
+
      debugShowCheckedModeBanner: false,
      home: SplashScreen(),
    );
